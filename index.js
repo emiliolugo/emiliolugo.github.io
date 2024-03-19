4775438e152dd6f726025c747249fae8d5ec1fcd
lugie314 <138060242+lugie314@users.noreply.github.com>
2024-03-20 02:37:29 +0400

    Finished Website
    Finished Website, missing project links, and new projects

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
     // Find the image element with id 'headshot'
     let headshot = document.getElementById('headshot');
     let info = document.getElementById('info');
     let title = document.getElementById('title');
     let map = document.getElementById('map');
     let projButton = document.getElementById('proj-button');
+    let resume = document.getElementById('resume');
     // Add a mouseenter event listener to the image element
     headshot.addEventListener('click', function() {
         // Change the image source when hovering over the image
+
+        headshot.style.backgroundColor = "#2B3035";
         headshot.classList.add('fade-image');
         setTimeout(function() {
             info.style.visibility = "visible";
-            info.classList.add("swipe");
+            info.classList.add("fade-left-across");
             setTimeout(function() {
                 title.style.visibility = "visible";
-                title.classList.add("swipe-center");
+                title.classList.add("fade-left");
                 setTimeout(function() {
                 projButton.style.visibility = "visible";
                 projButton.classList.add("fade-in");
+                setTimeout(function(){
+                    resume.style.visibility = "visible";
+                    resume.classList.add("fade-in");
+                },500);
                 },750);
 
             }, 750);
@@ -60,4 +68,65 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }, duration / (100 / increment));}
     });
+
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        var html = document.documentElement;
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || html.clientHeight) &&
+          rect.right <= (window.innerWidth || html.clientWidth)
+        );
+      }
+            // Function to handle scrolling and add swipe class to elements in viewport
+        function handleScroll() {
+            var hobby = document.getElementById("hobbies");
+            var projInfo = document.querySelectorAll('.proj-text');
+            var contact = document.getElementById('contact');
+            var job = document.getElementById('job-opps');
+            var contactInfo = document.getElementsByClassName('contact-sources');
+            
+            
+            projInfo.forEach(function(element) {
+                if (isInViewport(element)) {
+                    element.classList.add('fade-left');
+                }
+            });
+            if(isInViewport(hobby))
+            {
+                hobby.classList.add('fade-up');
+            }
+            if(isInViewport(contact))
+            {
+                contact.classList.add('fade-left');
+            }
+            if(isInViewport(contact))
+            {
+                setTimeout(function(){job.classList.add('fade-right');
+                setTimeout(function(){
+                    document.getElementById('connect').classList.add('fade-left');
+                    setTimeout(function(){
+                        for (var i = 0; i < contactInfo.length; i++) {
+                            (function(index) {
+                                setTimeout(function() {
+                                    contactInfo[index].classList.add('fade-up');
+                                }, 250 * index); // Apply a different delay for each iteration
+                            })(i);
+                        }
+
+                    },1000)
+                },1000)},1000)
+
+                
+                
+            }
+        }
+    
+        // Attach event listener for scroll event
+        window.addEventListener('scroll', handleScroll);
+    
+        // Initially call the function to add swipe class to elements in viewport on page load
+        window.addEventListener('load', handleScroll);
+    
 });
